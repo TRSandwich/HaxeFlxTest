701ce76c8d12fb9e2b6f512b489cfb5f96f16ec7
laimontr <128290019+TRSandwich@users.noreply.github.com>
2024-08-12 22:03:26 +0100

    Added Secret

--- HelloWorld/source/PlayState.hx
+++ HelloWorld/source/PlayState.hx
@@ -1,9 +1,13 @@
 package;
 
 import flixel.FlxState;
+import flixel.FlxG;
+import flixel.input.actions.FlxAction;
 
 class PlayState extends FlxState
 {
+	var testKey = new FlxActionDigital();
+	
 	override public function create()
 	{
 		super.create();
@@ -11,10 +15,14 @@ class PlayState extends FlxState
 		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
 		text.screenCenter();
 		add(text);
+
+		testKey.addKey(SPACE, JUST_PRESSED);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (testKey.check()) FlxG.switchState(new TestState());
 	}
 }
---
+++ HelloWorld/source/TestState.hx
@@ -0,0 +1,20 @@
+package;
+
+import flixel.FlxState;
+
+class TestState extends FlxState
+{
+    override public function create()
+    {
+        super.create();
+
+        var testtext = new flixel.text.FlxText(0, 0, 0, "SECRET THING", 64);
+        testtext.screenCenter();
+        add(testtext);
+    }
+
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+    }
+}
